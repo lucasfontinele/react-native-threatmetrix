bc0eb37171d6eb61c7954958338cdea649d3050b
Don Onwunumah <don.onwunumah@10xbanking.com>
2018-04-25 14:20:17 +0100

    removed unused constants

--- index.js
+++ index.js
@@ -6,13 +6,3 @@ export const RNThreatMetrix = Platform.select({
       ios: NativeModules.TMXReactNative,
       android: NativeModules.BlinkIDAndroid
 })
-
-/**
- * Following exports expose the keys (string constants) for obtaining result values for
- * corresponding result types.
- */
-export const MRTDKeys = require('./keys/mrtd_keys')
-export const USDLKeys = require('./keys/usdl_keys')
-export const EUDLKeys = require('./keys/eudl_keys')
-export const MYKADKeys = require('./keys/mykad_keys')
-export const PDF417Keys = require('./keys/pdf417_keys')
