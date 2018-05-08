e48cd1aaaf7f61da714db6255b87ca0ae5b00c5c
Don Onwunumah <don.onwunumah@10xbanking.com>
2018-05-08 14:24:19 +0100

    tidy up and remove dead-code

---
+++ publish.sh
@@ -0,0 +1,9 @@
+# TODO: scoped packages https://docs.npmjs.com/misc/scope
+
+# use internal registry
+npm config set registry http://repo.mylti3gh7p4x.net/repository/npm-mobile-releases/
+npm adduser --regsitry  http://repo.mylti3gh7p4x.net/repository/npm-mobile-releases/
+npm publish --regsitry http://repo.mylti3gh7p4x.net/repository/npm-mobile-releases/
+
+# reset to NPM 
+npm config set registry=http://registry.npmjs.org/
